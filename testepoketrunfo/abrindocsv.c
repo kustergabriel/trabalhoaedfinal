3929c99b62900f5b641885c4b0b9d4f24e4ab0fe
Gabriel Azevedo <gabrielazevedo1812@gmail.com>
2024-07-28 17:27:29 -0300

    deu certoooo

--- testepoketrunfo/abrindocsv.c
+++ testepoketrunfo/abrindocsv.c
@@ -2,28 +2,24 @@
 #include <string.h>
 #define Max_Linhas_Arq 1024
 
+
 int main() {
-    FILE *file = fopen("pokemon.csv", "r");
+    char line[Max_Linhas_Arq];
+    //tenta abrir o arquivo
+    FILE *file = fopen("/Meus Projetos/Trabalho Final AED1/trabalhoaedfinal/testepoketrunfo/pokemon.csv", "r");
     if (file == NULL) {
         printf("Erro ao abrir o arquivo pokemon.csv!\n");
         return 1;
     }
 
-    char line[Max_Linhas_Arq];
-
-    while (fgets(line, Max_Linhas_Arq, file)) {
-        // Remover a nova linha no final da linha (se houver)
-        line[strcspn(line, "\n")] = 0;
-
-        // Dividir a linha usando a vírgula como delimitador
-        char *token = strtok(line, ",");
-        while (token != NULL) {
-            printf("%s ", token);
-            token = strtok(NULL, ",");
-        }
-        printf("\n");
+    while (fgets(line, sizeof(line), file)) {
+        printf("%s", line);
     }
 
     fclose(file);
     return 0;
 }
+
+// function for open arquive
+
+void openarq ()

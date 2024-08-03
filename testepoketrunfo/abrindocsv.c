2cc619669cc487f47be17e4d0affab3a00890439
Gabriel Azevedo <gabrielazevedo1812@gmail.com>
2024-08-03 10:20:31 -0300

    AGORA FUNCIONANDO COM LISTAAA

--- testepoketrunfo/abrindocsv.c
+++ testepoketrunfo/abrindocsv.c
@@ -5,24 +5,32 @@
 #include <ctype.h>
 #include <time.h>
 
-/*criar o .c o .h e o makefile*/
-
 #define NRO_CARTAS 5 // numero de cartas por jogador
 #define MAX_POKEMONS 800 // nro de pokemons
 
+// vou utilizar listas para a pokedex!!!
+struct nodo{
+    int valor;
+    char linha[MAX_POKEMONS]; // armazenar a lista
+    struct nodo* prox;
+};
+typedef struct nodo Nodo;
+
 FILE *openarq(const char *filename);
-void free_lines(char *lines[], int count);
-void consultapokedex (char *linha[], int count);
+void consultapokedex(Nodo* lista);
 void telainicial (int *opcao);
 void telainicial0 (void);
-void shuffle(int array[], int n);
+void embaralharcartas(int array[], int n);
 void troca(int *a, int *b);
+Nodo* cria_lista(void);
+Nodo* insere_lista(Nodo* lista, char* linha);
+void imprime_lista(Nodo* lista);
+void free_lista(Nodo* lista);
 
 int main() {
-    char *linha[MAX_POKEMONS];  // armazenar as linhas do arquvio
-    char aux[MAX_POKEMONS]; // para duplicar a linha mesmo do arquivo
+    char aux[MAX_POKEMONS]; // para duplicar a linha do arquivo
     int telaload = 0;
-    int i = 0; 
+    Nodo* lista = cria_lista();
 
     //abre o arquivo e verifica se foi aberto corretamente.
     FILE *pokemon = openarq("/Meus Projetos/Trabalho Final AED1/trabalhoaedfinal/testepoketrunfo/pokemon.csv");
@@ -30,40 +38,23 @@ int main() {
         return 1; 
     }
 
-    // le todas as linhas e armazena no array de ponteiros
-    while (fgets(aux, sizeof(aux), pokemon) && i < MAX_POKEMONS) {    
-        linha[i] = strdup(aux); 
-        i++;
-    }
-
+    while (fgets(aux, sizeof(aux), pokemon)) { // le as linhas e armazena na lista
+        lista = insere_lista(lista, aux);
+}
     telainicial0(); // imprime a tela inicial
     
-    // tela de load do jogo
-    do {
+    do { // tela de load
         telainicial(&telaload);
         if (telaload == 0) {
-            consultapokedex(linha, i); /* acho que tenho que muda essa funcao para ser com listas!! */
+            consultapokedex(lista);  
         } else if (telaload == 1) {
             int cartas[MAX_POKEMONS];
             for (int j = 0; j < MAX_POKEMONS; j++) {
                 cartas[j] = j;
             }
 
-            /*  printf("Cartas antes do embaralhamento:\n");
-            for (int j = 0; j < NRO_CARTAS; j++) {
-                printf("%d ", cartas[j]);
-            }
-            printf("\n"); */
-
-            shuffle(cartas, MAX_POKEMONS); // aqui esta sendo aleatorizado
-            printf ("Cartas embaralhadas, o que deseja fazer agora? ");
-            printf("\n");
-
-           /* printf("Cartas embaralhadas:\n");
-            for (int j = 0; j < NRO_CARTAS; j++) {
-                printf("%s", linha[cartas[j]]);
-            }
-            printf("\n"); */
+            embaralharcartas(cartas, MAX_POKEMONS); // aqui esta sendo aleatorizado
+            printf ("Cartas embaralhadas, o que deseja fazer agora?\n");
 
         } else if (telaload == 2) {
             printf("Pensar no que vai ser feito!!\n");
@@ -71,13 +62,13 @@ int main() {
     } while (telaload != 10);
         printf("Jogo encerrado, obrigado por jogar!!!\n");
 
-    free_lines(linha, i); // liberando memoria das linhas
+    free_lista(lista); // liberando memoria das linhas
     fclose(pokemon); // fecha o arquivo .csv
 
     return 0;
 }
 
-    // abre o arquivo
+// abre o arquivo
 FILE *openarq(const char *filename) {
     FILE *pokemon = fopen(filename, "r");
     if (pokemon == NULL) {
@@ -87,15 +78,8 @@ FILE *openarq(const char *filename) {
     return pokemon;
 }
 
-    // funcao que libera a memoria para as linhas
-void free_lines(char *lines[], int count) {
-    for (int i = 0; i < count; i++) {
-        free(lines[i]); 
-    }
-}
-
-    // funcao para consultar a pokedex pelo nome do pokemon
-void consultapokedex (char *lines[], int count) {
+// funcao para consultar a pokedex pelo nome do pokemon
+void consultapokedex(Nodo* lista) {
     char nomepokemon[100];
     int encontrado = 0;
     printf("Insira o nome do Pokemon que deseja buscar: ");
@@ -104,23 +88,18 @@ void consultapokedex (char *lines[], int count) {
     for (int i = 0; nomepokemon[i]; i++) {
         nomepokemon[i] = toupper(nomepokemon[i]); // converte o nome do pokemon pra letras maisculas
     }
-
-    for (int j = 0; j < count; j++) {
-        char *linhaaux = strdup(lines[j]); // por que eu preciso duplicar???? 
-            if (linhaaux == NULL) {
-                printf("Erro de alocação de memória!\n");
-                break;
-            }
+    for (Nodo* p = lista; p != NULL; p = p->prox) {
+        char linhaaux[MAX_POKEMONS];
+        strcpy(linhaaux, p->linha);
         for (int k = 0; linhaaux[k]; k++) {
             linhaaux[k] = toupper(linhaaux[k]);
         }
-            if (strstr(linhaaux, nomepokemon) != NULL) {  /*strstr muito massa essa funcao!!! ela procura a string*/
-                printf("%s", lines[j]);
-                encontrado = 1;
-                free(linhaaux);
-                break;
-            } 
+        if (strstr(linhaaux, nomepokemon) != NULL) {
+            printf("%s", p->linha);
+            encontrado = 1;
+            break;
         }
+    }
         if (encontrado != 1)
             printf ("Pokemon nao encontrado!\n");
 }
@@ -148,10 +127,34 @@ void troca(int *a, int *b) {
     *b = temp;
 }
     // peguei o codigo da internet e implementei aqui
-void shuffle(int array[], int n) {
+void embaralharcartas(int array[], int n) {
     srand(time(NULL));
     for (int i = n - 1; i > 0; i--) {
         int j = rand() % (i + 1);
         troca(&array[i], &array[j]);
+    }
+}
+
+Nodo* cria_lista(void) {
+    return NULL;
+}
+
+Nodo* insere_lista(Nodo* lista, char* linha) {
+    Nodo* novo = (Nodo*) malloc(sizeof(Nodo));
+    if (novo == NULL) {
+        printf("Erro de alocação de memória!\n");
+        return lista;
+    }
+    strcpy(novo->linha, linha);
+    novo->prox = lista;
+    return novo;
+}
+
+void free_lista(Nodo* lista) {
+    Nodo* p = lista;
+    while (p != NULL) {
+        Nodo* temp = p->prox;
+        free(p);
+        p = temp;
     }
 }

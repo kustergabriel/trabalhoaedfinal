df1a1e9918cd5f3450f05bff4851ab8c5a010683
Gabriel Azevedo <gabrielazevedo1812@gmail.com>
2024-08-08 18:24:19 -0300

    agora com a pokedex mais bonita

--- testepoketrunfo/main.c
+++ testepoketrunfo/main.c
@@ -1,4 +1,3 @@
-#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,8 +8,7 @@
 #define MAX_POKEMONS 800 // nro de pokemons
 
 // vou utilizar listas para a pokedex!!!
-struct nodo{
-    int valor;
+struct nodo {
     char linha[MAX_POKEMONS]; // armazenar a lista
     struct nodo* prox;
 };
@@ -26,12 +24,13 @@ Nodo* cria_lista(void);
 Nodo* insere_lista(Nodo* lista, char* linha);
 void imprime_lista(Nodo* lista);
 void free_lista(Nodo* lista);
+void pokeinfo(char *linha);
 
 int main() {
     char aux[MAX_POKEMONS]; // para duplicar a linha do arquivo
     int telaload = 0;
     Nodo* lista = cria_lista();
-
+    
     //abre o arquivo e verifica se foi aberto corretamente.
     FILE *pokemon = openarq("/Meus Projetos/Trabalho Final AED1/trabalhoaedfinal/testepoketrunfo/pokemon.csv");
     if (pokemon == NULL) {
@@ -40,7 +39,8 @@ int main() {
 
     while (fgets(aux, sizeof(aux), pokemon)) { // le as linhas e armazena na lista
         lista = insere_lista(lista, aux);
-}
+    }
+    
     telainicial0(); // imprime a tela inicial
     
     do { // tela de load
@@ -54,14 +54,15 @@ int main() {
             }
 
             embaralharcartas(cartas, MAX_POKEMONS); // aqui esta sendo aleatorizado
-            printf ("Cartas embaralhadas, o que deseja fazer agora?\n");
+            printf ("Cartas embaralhadas JOGADOR 1, o que deseja fazer agora?\n");
             
 
         } else if (telaload == 2) {
             printf("Pensar no que vai ser feito!!\n");
         }
     } while (telaload != 10);
-        printf("Jogo encerrado, obrigado por jogar!!!\n");
+    
+    printf("Jogo encerrado, obrigado por jogar!!!\n");
 
     free_lista(lista); // liberando memoria das linhas
     fclose(pokemon); // fecha o arquivo .csv
@@ -96,38 +97,64 @@ void consultapokedex(Nodo* lista) {
             linhaaux[k] = toupper(linhaaux[k]);
         }
         if (strstr(linhaaux, nomepokemon) != NULL) {
-            printf("%s", p->linha);
+            pokeinfo(p->linha);
             encontrado = 1;
             break;
         }
     }
-        if (encontrado != 1)
-            printf ("Pokemon nao encontrado!\n");
+    if (!encontrado)
+        printf ("Pokemon nao encontrado!\n");
 }
 
-    // tela inicial mesmo mesmo
-void telainicial0 (void) {
-    printf("----------------------Seja bem vindo ao POKETRUNFO!----------------------\n");
-    printf("----------------------Insira uma tecla para Comecar a jogar----------------------\n");
+void linhazinha(int width) {
+    for (int i = 0; i < width; i++) {
+        printf("-");
+    }
+    printf("\n");
 }
 
-    // funcao da tela inicial
-void telainicial (int *opcao) {
-    printf("---------------------- 0 = CONSULTA POKEDEX----------------------\n");
-    printf("---------------------- 1 = EMBARALHAR AS CARTAS----------------------\n");
-    printf("---------------------- 2 = SORTEIO DAS CARTAS----------------------\n");
-    printf("---------------------- 10 = SAIR DO JOGO----------------------\n");
+void colocarnomeio(const char *text, int width) {
+    int padding = (width - strlen(text)) / 2;
+    printf("|");
+    for (int i = 0; i < padding; i++) {
+        printf(" ");
+    }
+    printf("%s", text);
+    for (int i = 0; i < padding; i++) {
+        printf(" ");
+    }
+    printf("|\n");
+}
+
+void telainicial0(void) {
+    int width = 50;
+    linhazinha(width);
+    colocarnomeio("Seja bem vindo ao POKETRUNFO!", width);
+    linhazinha(width);
+    colocarnomeio("Insira uma tecla para comecar a jogar", width);
+    linhazinha(width);
+}
+
+void telainicial(int *opcao) {
+    int width = 50;
+    linhazinha(width);
+    colocarnomeio("0 = CONSULTA POKEDEX", width);
+    colocarnomeio("1 = EMBARALHAR AS CARTAS", width);
+    colocarnomeio("2 = SORTEIO DAS CARTAS", width);
+    colocarnomeio("10 = SAIR DO JOGO", width);
+    linhazinha(width);
     printf("Insira a sua alternativa: ");
     scanf("%d", opcao);
 }
 
-    // vou utilizar o fisher yates  
+// vou utilizar o fisher yates  
 void troca(int *a, int *b) {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
-    // peguei o codigo da internet e implementei aqui
+
+// peguei o codigo da internet e implementei aqui
 void embaralharcartas(int array[], int n) {
     srand(time(NULL));
     for (int i = n - 1; i > 0; i--) {
@@ -143,7 +170,6 @@ Nodo* cria_lista(void) {
 Nodo* insere_lista(Nodo* lista, char* linha) {
     Nodo* novo = (Nodo*) malloc(sizeof(Nodo));
     if (novo == NULL) {
-        printf("Erro de alocação de memória!\n");
         return lista;
     }
     strcpy(novo->linha, linha);
@@ -158,4 +184,78 @@ void free_lista(Nodo* lista) {
         free(p);
         p = temp;
     }
+}
+
+void pokeinfo(char *linha) {
+    char *token;
+    char *delim = ","; // ou o delimitador que separa os campos no  CSV
+    int field = 0;
+    
+    char linhaCopia[MAX_POKEMONS];
+    strcpy(linhaCopia, linha); // Cria uma copia para n modificar a linha original
+    
+    printf("\n");
+    linhazinha(50);
+    token = strtok(linhaCopia, delim);
+    while (token != NULL) {
+        switch (field) {
+                case 0:
+                printf("#: %s\n", token);
+                break;
+
+                case 1:
+                printf("Nome: %s\n", token);
+                break;
+
+                case 2:
+                printf("Type 1: %s\n", token);
+                break;
+
+                 case 3:
+                printf("Type 2: %s\n", token);
+                break;
+                
+                case 4:
+                printf("Total: %s\n", token);
+                break;
+
+                case 5:
+                printf("HP: %s\n", token);
+                break;
+
+                case 6:
+                printf("Attack: %s\n", token);
+                break;
+
+                case 7:
+                printf("Defense: %s\n", token);
+                break;
+
+                case 8:
+                printf("Sp. Atk: %s\n", token);
+                break;
+
+                case 9:
+                printf("Sp. Def: %s\n", token);
+                break;
+
+                case 10:
+                printf("Speed: %s\n", token);
+                break;
+
+                case 11:
+                printf("Generation: %s\n", token);
+                break;
+
+                case 12:
+                printf("Legendary: %s\n", token);
+                break;
+
+               
+        }
+       field++;
+        token = strtok(NULL, delim);
+    }
+    linhazinha(50);
+    printf("\n");
 }
